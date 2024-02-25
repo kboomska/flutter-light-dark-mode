e5f7a13a108959ee24d53fe79abeb838bce90eab
Aleksey Kuzmin <kboomska@gmail.com>
2024-02-25 12:00:41 +0300

    Changed colorSheme

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,12 +9,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[500],
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
         child: MyBox(
-          color: Colors.blue[300],
+          color: Theme.of(context).colorScheme.primary,
           child: MyButton(
-            color: Colors.blue[200],
+            color: Theme.of(context).colorScheme.secondary,
             onTap: () {},
           ),
         ),
